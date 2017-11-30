8d8cbba9c4973577d272076654d447742f6d9145
Tomas Sardyha <darsain@gmail.com>
2017-11-30 11:54:03 +0100

    Ugly hacks to help with Chrome invisible iframe rendering issue

--- src/js/boot/chat.js
+++ src/js/boot/chat.js
@@ -3,3 +3,14 @@ var channel = match ? match[1].toLowerCase() : null;
 var iframe = document.querySelector('#chat');
 iframe.src = 'https://www.twitch.tv/' + channel + '/chat';
 document.querySelector('title').textContent = 'TGA: ' + channel;
+
+// stupid chrome rendering bugs
+// If I won't do this the chat refuses to render and looks invisible, even
+// though it's loaded. Still doesn't work 100% of the time :/
+setTimeout(function () {
+	var pos = iframe.style.position;
+	iframe.style.position = 'static';
+	setTimeout(function () {
+		iframe.style.position = pos;
+	}, 100);
+}, 600);
